75aeb22be3d6a4776715b320684863af5d110cf4
sbodily-git <sbodily@att.net>
2021-09-24 00:03:48 -0600

    Initial committ

---
+++ complete.R
@@ -0,0 +1,16 @@
+complete <- function(direcotry, id = 1:332) {
+	## 'directory' is a charaacter vector of length 1 indicating
+	## the location of the csv files
+	
+	## 'id' is an integer vector indicating the monitor ID numbers
+	## to be used.
+	
+	## Return a data frame of the form:
+	## id nobs
+	## 1  117
+	## 2  1047
+	## ...
+	## where 'id' is the monitor ID number and 'nobs' is the
+	## number of complete cases.
+	
+} # end complete
